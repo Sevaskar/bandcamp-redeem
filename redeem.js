bbd4d1758f38b926101f89db7e9b95474a7c635e
Sevaskar <dxwx@protonmail.com>
2025-03-29 17:27:38 -0700

    Update redeem.js

--- redeem.js
+++ redeem.js
@@ -89,28 +89,3 @@ async function updateButtons() {
         }
     }
 }
-
-document.addEventListener("DOMContentLoaded", updateButtons);
-document.addEventListener("DOMContentLoaded", function () {
-    let bandcampURL = localStorage.getItem("bandcampURL");
-
-    if (!bandcampURL) {
-        bandcampURL = prompt("Please enter your Bandcamp URL:");
-
-        if (!bandcampURL) {
-            alert("You need to enter your Bandcamp URL to continue.");
-            return;
-        }
-
-        localStorage.setItem("bandcampURL", bandcampURL);
-    }
-
-    checkRedemptions(bandcampURL);
-
-    document.querySelectorAll(".redeem-btn").forEach(button => {
-        button.addEventListener("click", function () {
-            const album = this.getAttribute("data-album");
-            redeemCode(bandcampURL, album, this);
-        });
-    });
-});
